f5e4338e785da6600517b39d50f78846921e51ff
kenergeo <>
2021-04-13 03:01:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+# Simple snake game in python 3 or 2 
+# Aplril 12 2021
+# Part 1: the set up
+
+import turtle
+import time
+
+delay = 0.1
+
+# Set up the screen
+gameboard = turtle.Screen()
+gameboard.title("Snake PARTY By reg5060")
+gameboard.bgcolor("blue")
+gameboard.setup(width=600, height=600)
+gameboard.tracer(0)
+
+# Snake head
+snake = turtle.Turtle()
+snake.speed(0)
+snake.shape("square")
+snake.color("brown")
+snake.penup()
+snake.goto(0,0)
+snake.direction = "stop"
+
+
+# Functions
+def go_up():
+    snake.direction = "up"
+
+def go_down():
+    snake.direction = "down"
+
+def go_left():
+    snake.direction = "left"
+
+def go_right():
+    snake.direction = "right"
+
+def move():
+    if snake.direction == "up":
+      y = snake.ycor()
+      snake.sety(y + 20)
+
+    if snake.direction == "down":
+      y = snake.ycor()
+      snake.sety(y - 20)
+
+    if snake.direction == "left":
+      x = snake.xcor()
+      snake.setx(x - 20)
+
+    if snake.direction == "right":
+      x = snake.xcor()
+      snake.setx(x + 20)
+
+#Keyboard bindings
+gameboard.listen()
+gameboard.onkeypress(go_up, "Up")
+gameboard.onkeypress(go_right, "Right")
+gameboard.onkeypress(go_left, "Left")
+gameboard.onkeypress(go_down, "Down")
+
+# Main game loop
+while True:
+    gameboard.update()
+
+    move()
+
+    time.sleep(delay)
+
+gameboard.mainloop()
