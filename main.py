b6ded82b7d841a4265d5e5849b2375cd22e55729
kenergeo <>
2021-04-15 17:03:56 +0000

    add to snake body

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ snake.goto(0,0)
 snake.direction = "stop"
 
 #Food
-
 food = turtle.Turtle()
 food.speed(0)
 food.shape("circle")
@@ -33,6 +32,7 @@ food.color("red")
 food.penup()
 food.goto(0,100)
 
+segments = []
 
 # Functions
 def go_up():
@@ -75,11 +75,32 @@ gameboard.onkeypress(go_down, "Down")
 while True:
     gameboard.update()
 
+    #check for collision with the food
     if snake.distance(food) < 20:
         # Move the food to a random spot
         x = random.randint(-290, 290)
         y = random.randint(-290, 290)
         food.goto(x,y)
+
+        #Add a new segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("circle")
+        new_segment.color("yellow")
+        new_segment.penup()
+        segments.append(new_segment)
+
+    #Move the end segments first in reverse open
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+    #Move segment 0 to where the snake is
+    if len(segments) > 0:
+        x = snake.xcor()
+        y = snake.ycor()
+        segments[0].goto(x,y)
 
     move()
 
